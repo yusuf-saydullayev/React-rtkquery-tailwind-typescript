aa0c979f1a69accf84828026e36492a558c1d77f
Yusuf <yusufsaydullayev@gmail.com>
2022-08-04 18:37:48 +0500

    Simple changes to be committed

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,23 +1,21 @@
 import { FC } from 'react'
 import { useGetAllUsersQuery } from '../Redux/User.api';
-import { User } from '../Types/Posts'
+import { Post } from '../Types/Posts'
 
 const Home: FC = () => {
   const { data, isLoading, error } = useGetAllUsersQuery('posts')
 
   return (
     <div className='flex justify-center py-6'>
-      {isLoading ? (
-        "Loading...."
-      ) : error ? (
-        "Error"
-      ) : data ? (
+      {error && 'Error'}
+      {isLoading && "Loading...."}
+      {data && (
         <ul>
-          {data.map((item: User) => {
+          {data.map((item: Post) => {
             return <li key={item.id} className="text-center text-white font-mono bg-lime-500 rounded-md my-2 hover:bg-cyan-600 transition delay-50 hover:scale-105 py-1">{item.title}</li>;
           })}
         </ul>
-      ) : null}
+      )}
     </div>
   )
 }
--- src/Redux/User.api.ts
+++ src/Redux/User.api.ts
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Post } from '../Types/Posts';
 
 export const apiUser = createApi({
   reducerPath: "Userapi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com/" }),
   endpoints: (builder) => ({
-    getAllUsers: builder.query<any, string>({
+    getAllUsers: builder.query<Post[], string>({
       query: (url) => `${url}`
     })
   })
